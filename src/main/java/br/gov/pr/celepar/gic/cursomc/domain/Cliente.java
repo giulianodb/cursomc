00a53587e9eec0e1c47b942f68fb7e6497d2912f
Giuliano Diego Barbarra <giulianodb@gmail.com>
2018-06-20 18:42:26 -0300

    endpoint cliente disponivil

--- src/main/java/br/gov/pr/celepar/gic/cursomc/domain/Cliente.java
+++ src/main/java/br/gov/pr/celepar/gic/cursomc/domain/Cliente.java
@@ -15,6 +15,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import br.gov.pr.celepar.gic.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -32,6 +34,7 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
