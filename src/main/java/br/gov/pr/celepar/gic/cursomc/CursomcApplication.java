5aa2938443f6b57a37631e8017992440cc15e904
Giuliano Diego Barbarra <giulianodb@gmail.com>
2018-06-20 16:15:26 -0300

    Adicionar produto e categoria

--- src/main/java/br/gov/pr/celepar/gic/cursomc/CursomcApplication.java
+++ src/main/java/br/gov/pr/celepar/gic/cursomc/CursomcApplication.java
@@ -1,11 +1,16 @@
 package br.gov.pr.celepar.gic.cursomc;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.gov.pr.celepar.gic.cursomc.domain.Categoria;
+import br.gov.pr.celepar.gic.cursomc.domain.Produto;
+import br.gov.pr.celepar.gic.cursomc.repositories.CategoriaRepository;
+import br.gov.pr.celepar.gic.cursomc.repositories.ProdutoRepository;
 import br.gov.pr.celepar.gic.cursomc.services.CategoriaService;
 
 @SpringBootApplication
@@ -14,6 +19,11 @@ public class CursomcApplication implements CommandLineRunner{
 	@Autowired
 	private CategoriaService categoriaService;
 	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -24,8 +34,24 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		categoriaService.cadastrar(cat1);
 		categoriaService.cadastrar(cat2);
+		
+//		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		// 
 		
 	}
 }
