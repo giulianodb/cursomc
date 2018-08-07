ff38e98b045aefc6e016d806717f947f0faa4ce0
Giuliano Diego Barbarra <giulianodb@gmail.com>
2018-08-07 16:55:44 -0300

    Validação customizada de email já existente na inserção

--- src/main/java/br/gov/pr/celepar/gic/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/gov/pr/celepar/gic/cursomc/services/validation/ClienteInsertValidator.java
@@ -5,17 +5,28 @@ import java.util.List;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import javax.validation.ReportAsSingleViolation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.gov.pr.celepar.gic.cursomc.domain.Cliente;
 import br.gov.pr.celepar.gic.cursomc.domain.enums.TipoCliente;
 import br.gov.pr.celepar.gic.cursomc.dto.ClienteNewDTO;
+import br.gov.pr.celepar.gic.cursomc.repositories.ClienteRepository;
 import br.gov.pr.celepar.gic.cursomc.resources.exceptions.FieldMessage;
 import br.gov.pr.celepar.gic.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
-
+	
+	
+	
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
@@ -27,6 +38,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux !=null) {
+			list.add( new FieldMessage("email","Email já existente"));
 		}
 		
 		// inclua os testes aqui, inserindo erros na lista
