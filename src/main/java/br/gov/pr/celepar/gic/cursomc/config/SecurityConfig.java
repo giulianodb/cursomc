427aca98c06cdba18682f1e08c0cc10c60793f2b
Giuliano Diego Barbarra <giulianodb@gmail.com>
2018-09-19 15:36:08 -0300

    Adicionando a authorization

--- src/main/java/br/gov/pr/celepar/gic/cursomc/config/SecurityConfig.java
+++ src/main/java/br/gov/pr/celepar/gic/cursomc/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.gov.pr.celepar.gic.cursomc.security.JWTAuthenticationFilter;
+import br.gov.pr.celepar.gic.cursomc.security.JWTAuthorizationFilter;
 import br.gov.pr.celepar.gic.cursomc.security.JWTUtil;
 
 @Configuration
@@ -43,6 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService));
+		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
